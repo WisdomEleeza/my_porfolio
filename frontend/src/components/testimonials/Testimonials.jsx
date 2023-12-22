934ee43539de6e7b932d4ae1e7e149cb99d7de15
Wisdom Eleeza <whiy07@gmail.com>
2023-12-22 11:04:55 +0000

    work on testimonial

--- frontend/src/components/testimonials/Testimonials.jsx
+++ frontend/src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import data from "./data";
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
-}
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
 
-export default Testimonials
+      <div className="container testimonials__container">
+        {data.map(({ avatar, name, review }) => {
+          return (
+            <article className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
