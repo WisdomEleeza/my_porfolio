26450cdb7b975588a3faf7e7529822217d39df51
Wisdom Eleeza <whiy07@gmail.com>
2023-12-22 11:39:10 +0000

    Work on sliding using swipper

--- frontend/src/components/testimonials/Testimonials.jsx
+++ frontend/src/components/testimonials/Testimonials.jsx
@@ -2,25 +2,40 @@ import React from "react";
 import "./testimonials.css";
 import data from "./data";
 
+// Import Swiper core and required modules
+import { Pagination, Navigation, Scrollbar } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
 const Testimonials = () => {
   return (
     <section id="testimonials">
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
-        {data.map(({ avatar, name, review }) => {
+      <Swiper
+        className="container testimonials__container"
+        modules={[Pagination, Navigation, Scrollbar]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
           return (
-            <article className="testimonial">
+            <SwiperSlide key={index} className="testimonial">
               <div className="client__avatar">
                 <img src={avatar} alt="" />
               </div>
               <h5 className="client__name">{name}</h5>
               <small className="client__review">{review}</small>
-            </article>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
