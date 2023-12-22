ab8e238526cf131c32ee77f1c4b3bcfabb1e2438
Wisdom Eleeza <whiy07@gmail.com>
2023-12-22 09:45:43 +0000

    Work styling and maping through the data array object

--- frontend/src/components/porfolio/Portfolio.jsx
+++ frontend/src/components/porfolio/Portfolio.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/portfolio1.jpg";
-import IMG2 from "../../assets/portfolio2.jpg";
-import IMG3 from "../../assets/portfolio3.jpg";
-import IMG4 from "../../assets/portfolio4.jpg";
-import IMG5 from "../../assets/portfolio5.png";
-import IMG6 from "../../assets/portfolio6.jpg";
+import data from "./data";
 
 const Portfolio = () => {
   return (
@@ -14,150 +9,34 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="porfolio__item-image">
-            <img src={IMG1} alt="Portfolio Item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/WisdomEleeza/"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://dribble.com/Alien_pixels"
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-primary"
-            >
-              Live Demo"
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="porfolio__item-image">
-            <img src={IMG1} alt="Portfolio Item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/WisdomEleeza/"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://dribble.com/Alien_pixels"
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-primary"
-            >
-              Live Demo"
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="porfolio__item-image">
-            <img src={IMG1} alt="Portfolio Item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/WisdomEleeza/"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://dribble.com/Alien_pixels"
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-primary"
-            >
-              Live Demo"
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="porfolio__item-image">
-            <img src={IMG1} alt="Portfolio Item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/WisdomEleeza/"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://dribble.com/Alien_pixels"
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-primary"
-            >
-              Live Demo"
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="porfolio__item-image">
-            <img src={IMG1} alt="Portfolio Item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/WisdomEleeza/"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://dribble.com/Alien_pixels"
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-primary"
-            >
-              Live Demo"
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="porfolio__item-image">
-            <img src={IMG1} alt="Portfolio Item" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/WisdomEleeza/"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://dribble.com/Alien_pixels"
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-primary"
-            >
-              Live Demo"
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="porfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-primary"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
